a7d6645cecb9fbab744ec1b32641d8d9fa973bce
Jason Coulls <coulls@gmail.com>
2023-11-04 21:30:55 -0400

    Bug fixed dark mode on the title bar.

--- LogBazooka/DarkMode.cpp
+++ LogBazooka/DarkMode.cpp
@@ -5,27 +5,22 @@
 // This class handles dark mode routines for the application.
 
 bool ShouldAppsUseDarkMode() {
+    // Check if the user has enabled dark mode.
+    HKEY hKey;
+    LSTATUS status = RegOpenKeyExW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", 0, KEY_READ, &hKey);
+    if (status == ERROR_SUCCESS) {
+        DWORD value = 0;  // Initialize value
+        DWORD valueSize = sizeof(DWORD);
+        status = RegQueryValueExW(hKey, L"AppsUseLightTheme", NULL, NULL, (LPBYTE)&value, &valueSize);
+        RegCloseKey(hKey);
+        if (status == ERROR_SUCCESS) {
+            // If the registry value for light theme is 0, dark mode is enabled, so return true.
+            return value == 0;
+        }
+    }
 
-	// Check if the user has enabled dark mode.
-	HKEY hKey;
-	LSTATUS status = RegOpenKeyExW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", 0, KEY_READ, &hKey);
-	if (status == ERROR_SUCCESS) {
-
-		DWORD value;
-		DWORD valueSize = sizeof(DWORD);
-		status = RegQueryValueExW(hKey, L"AppsUseLightTheme", NULL, NULL, (LPBYTE)&value, &valueSize);
-		RegCloseKey(hKey);
-		if (status == ERROR_SUCCESS) {
-
-			// If the user has enabled dark mode, return true.
-			if (value == 0) {
-				return true;
-			}
-		}
-	}
-
-	// If the user has not enabled dark mode, return false.
-	return false;
+    // If the user has not enabled dark mode or there was an error reading the registry, return false.
+    return false;
 }
 
 
@@ -33,5 +28,7 @@ void EnableDarkModeForApp(HWND hwnd) {
 	// Set window to dark mode
 	SetWindowTheme(hwnd, L"DarkMode_Explorer", NULL);
 	//DwmSetWindowAttribute(hwnd, DWMWA_USE_IMMERSIVE_DARK_MODE, &(BOOL) {TRUE}, sizeof(BOOL));
+	BOOL isDarkMode = TRUE;
+	DwmSetWindowAttribute(hwnd, DWMWA_USE_IMMERSIVE_DARK_MODE, &isDarkMode, sizeof(isDarkMode));
 }
 
